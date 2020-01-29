2b8e3382a514a9ebb3cadd78f7cacc82e67580ef
santhoshkumar22101999 <60419124+santhoshkumar22101999@users.noreply.github.com>
2020-01-29 14:24:04 +0530

    Loop_or_Dont.py
    1.user enters a character
    2.the looping starts then we kill it.
    3.if user enters q as input, the looping never occur

---
+++ Loop_or_Dont.py
@@ -0,0 +1,24 @@
+a=raw_input("enter a char\n")
+while(True):
+    print("a")
+    if(a=='q'):
+        break
+
+'''
+SAMPLE OUTPUT:
+enter a char
+a
+
+a
+a
+a
+.
+.
+.
+-----------------
+enter a char 
+q
+
+a
+
+'''
